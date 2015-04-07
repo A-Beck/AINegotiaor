a9475ed0eb046dc024cc6234dccae23ee9bdbdf9
A-Beck <adb2jb@virginia.edu>
2015-04-07 17:01:04 -0400

    Merging my AI w/ Monty's Code to be in line w/ floryian

--- adb2jb.py
+++ adb2jb.py
@@ -19,6 +19,7 @@ class Negotiator(BaseNegotiator):
         self.utility_history = {}  # keeps track of past opponent utilities
         self.offer_history = {}  # keeps track of past opponent offers
         self.past_offers = []  # keeps track of our past offers
+        self.goes_last = True
 
     def make_offer(self, offer):
         #set up history data structure for current round
@@ -27,8 +28,9 @@ class Negotiator(BaseNegotiator):
         if self.round_number not in self.offer_history.keys():
             self.offer_history[self.round_number] = {}
         if offer is None:
-            # Need to make a first offer to start negotiations
+            # Need to make a first offer to start negotiations, does not go last
             # This offer does not count towards the number of negotiations
+            self.goes_last = False
             offer = self.make_first_offer()
             self.past_offers.append(offer)
         elif self.current_iter == self.iter_limit:
@@ -46,22 +48,45 @@ class Negotiator(BaseNegotiator):
         return offer
 
     def make_first_offer(self):
-        print('first offer')
+        # print('first offer')
         return self.preferences
 
     def make_intermediate_offer(self, offer):
-        print('in middle of negotiations')
+        # print('in middle of negotiations')
         if offer in self.past_offers:
             self.past_offers = []  # this round of our negotiation is over, we can clear our offer history
             return offer
-        return self.preferences
+        else:
+            self.offer = self.one_up(offer)
+            return self.offer
 
     def make_final_offer(self, offer):
-        print('in final inter')
+        # print('in final inter')
         self.past_offers = []  # this round of our negotiation is over, we can clear our offer history
         if offer in self.past_offers:
             return offer
-        return self.preferences
+        else:
+            self.offer = self.one_up(offer)
+            return self.offer
+
+    def one_up(self, opponent_offer):
+        if opponent_offer is None:
+            return self.offer
+        if self.offer == opponent_offer:
+            return self.offer
+        starting_index = 0
+        while self.offer[starting_index] == opponent_offer[starting_index]:
+            if starting_index == len(self.offer) - 1:
+                break
+            starting_index += 1
+        swap_index = starting_index
+        while True:
+            if self.offer[swap_index] == opponent_offer[starting_index]:
+                break
+            else:
+                swap_index += 1
+        self.offer[starting_index], self.offer[swap_index] = self.offer[swap_index], self.offer[starting_index]
+        return self.offer
 
     def receive_utility(self, utility):
         # recieves the utility from the previous negotiation and puts it into the right place in history
@@ -70,6 +95,9 @@ class Negotiator(BaseNegotiator):
         pass
 
     def calc_utility(self, offer):
-        total = len(self.preferences)
-        util = reduce(lambda points, item: points + ((total / (offer.index(item) + 1)) - abs(offer.index(item) - self.preferences.index(item))), offer, 0)
+        # This is the way florian said to do it in class on 4/7
+        old_offer = self.offer
+        self.offer = offer
+        util = self.utility()
+        self.offer = old_offer
         return util
